58420ad488b32069b75e0ad3dfe3213a6978fa48
kevinkluivertfelix <123820179+kevinkluivertfelix@users.noreply.github.com>
2023-02-25 10:41:59 -0300

    update folders structure

---
+++ Assets/Scripts/GunScripts/Bullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float spreadFactor;
+    public int damage;
+    private float angleMod;
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+        rb = GetComponent<Rigidbody2D>();
+        var rot = transform.rotation;
+        transform.rotation = rot * Quaternion.Euler(0, 0, Random.Range(-spreadFactor, spreadFactor)); // t$$anonymous$$s is 90 degrees around y axis
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.right * speed * Time.deltaTime;
+    }
+}
